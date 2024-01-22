e1847f5c9f10db031f050d6df35969c7e2524104
Tyler Dinh <92825742+tylurr@users.noreply.github.com>
2024-01-22 15:55:09 -0600

    Update and rename LimeLight.java to Limelight.java

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,4 +7,4 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /** Add your docs here. */
-public class LimeLight extends SubsystemBase {}
+public class Limelight extends SubsystemBase {}
