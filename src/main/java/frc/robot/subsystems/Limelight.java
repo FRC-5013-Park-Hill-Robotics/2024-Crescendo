e3afa0c1329187af66bf92f6cdf7d6af02e55fa7
Tyler Dinh <tylerpokemon3@gmail.com>
2024-02-03 16:30:21 -0600

    CAN Constants + basic pose w/april tag using LL

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,11 +4,42 @@
 
 package frc.robot.subsystems;
 
+import java.io.IOException;
+import java.sql.Driver;
+import java.util.Optional;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
+
+  AprilTagFieldLayout aprilTagFieldLayout;
+
   /** Creates a new Limelight. */
   public Limelight() {}
+  
+  public void initialize(){
+    Optional<Alliance> alliance = DriverStation.getAlliance();
+    if(alliance.isPresent()){
+        if (aprilTagFieldLayout == null) {
+            aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+        }
+        if (alliance.get() == Alliance.Red) {
+            aprilTagFieldLayout.setOrigin(OriginPosition.kRedAllianceWallRightSide);
+        } 
+        else {
+            aprilTagFieldLayout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
+        } 
+        //m_leftPoseEstimator =new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP, m_leftCamera,LeftCamera.robotToCam );
+        //m_rightPoseEstimator =new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP, m_rightCamera,RightCamera.robotToCam );
+
+        //isinitialized = true;
+    }
+}
 
   @Override
   public void periodic() {
