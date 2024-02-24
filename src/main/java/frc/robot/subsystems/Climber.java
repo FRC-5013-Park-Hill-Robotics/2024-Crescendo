877af67abd3288c4bba56d194ae9b9ad178d635c
Tyler Dinh <tylerpokemon3@gmail.com>
2024-02-24 15:14:06 -0600

    climber and climber commands

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.CommandSwerveDrivetrain;
+import frc.robot.RobotContainer;
 import frc.robot.constants.CANConstants;
 import frc.robot.constants.ClimberConstants;
 import frc.robot.constants.ClimberConstants;
@@ -19,6 +22,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class Climber extends SubsystemBase {
@@ -26,6 +30,7 @@ public class Climber extends SubsystemBase {
   private final TalonFX rightClimberMotor = new TalonFX(CANConstants.CLIMBER_RIGHT_CAN_ID);
   private ProfiledPIDController climberController = new ProfiledPIDController(ClimberConstants.kP, ClimberConstants.kI, ClimberConstants.kD, null);
   private VoltageOut voltage = new VoltageOut(0, false, false, false, false);
+  private CommandXboxController operatorController = RobotContainer.getInstance().getOperatorController();
 
   //private VelocityVoltage m_BottomVoltage = new VelocityVoltage(0);
   //private VelocityVoltage m_topVoltage = new VelocityVoltage(0);
@@ -36,18 +41,10 @@ public class Climber extends SubsystemBase {
 
   /** Creates a new Climber. */
   public Climber() {
-    //factory default left leftClimberMotor.configFactoryDefault();
-    //factory default right leftClimberMotor.configFactoryDefault();
-
-    //TODO: Config right motor
-
-    rightClimberMotor.getConfigurator().apply(new TalonFXConfiguration());
 
     //TODO: change inversions to correct values
-    rightClimberMotor.setInverted(true);
 
-    rightClimberMotor.setNeutralMode(NeutralModeValue.Brake);
-
+    //limit switch?
     /* 
     HardwareLimitSwitchConfigs limitSwitchConfigs = new HardwareLimitSwitchConfigs();
     leftClimberMotor.getForwardLimit();
@@ -57,17 +54,18 @@ public class Climber extends SubsystemBase {
 		leftClimberMotor.getReverseLimit();
 
     */
+
     TalonFXConfiguration leftConfig = new TalonFXConfiguration();
-    leftConfig.Slot0.kP = ClimberConstants.kP;
-    leftConfig.Slot0.kI = ClimberConstants.kI;
-    leftConfig.Slot0.kD = ClimberConstants.kD;
-    leftConfig.Slot0.kS = ClimberConstants.kS;
-    leftConfig.Slot0.kV = ClimberConstants.kV;
-    leftConfig.Slot0.kA = ClimberConstants.kA;
     leftClimberMotor.setNeutralMode(NeutralModeValue.Brake);
     leftClimberMotor.set(0);
     leftClimberMotor.setInverted(true);
     leftClimberMotor.getConfigurator().apply(leftConfig);
+
+    TalonFXConfiguration rightConfig = new TalonFXConfiguration();
+    rightClimberMotor.setNeutralMode(NeutralModeValue.Brake);
+    rightClimberMotor.set(0);
+    rightClimberMotor.setInverted(true);
+    rightClimberMotor.getConfigurator().apply(rightConfig);
     //m_leftVoltage.withSlot(0);
 
 
@@ -82,28 +80,49 @@ public class Climber extends SubsystemBase {
 
   }
 
-  public void extendUp(){
-
+  public TalonFX getLeftMotor() {
+    return this.leftClimberMotor;
   }
 
-  public void extendDown(){
-
+  public TalonFX getRightMotor() {
+    return this.rightClimberMotor;
   }
 
+  
+  public void leftClimberMove(double stickInput){
+    leftClimberMotor.set(stickInput);
+  }
 
+  public void rightClimberMove(double stickInput){
+    rightClimberMotor.set(stickInput);
+  }
+  
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  public Command getClimbComand(){
+  public Command climbLeftCommand(double stickInput){
+    Command result = run(() -> leftClimberMove(stickInput));
+    return result;
+
     //TODO make climb command
     //apply 9.6 v to the low motor and 9.6 minus abssolute value of the pid for the high motor. +   gravity feed forward
     //stop when one motor hits extend limit switch
-    return null;
   }
-  public Command reseteClimberComand(){
+
+  public Command climbRightCommand(double stickInput){
+    Command result = run(() -> rightClimberMove(stickInput));
+    return result;
+
+    //TODO make climb command
+    //apply 9.6 v to the low motor and 9.6 minus abssolute value of the pid for the high motor. +   gravity feed forward
+    //stop when one motor hits extend limit switch
+  }
+
+
+  public Command resetClimberCommand(){
         //TODO make climb command
     //apply - 3 v +  feed forward
     //stop when one motor hits extend limit switch (if we don't have lower limit when amperage spikes)
