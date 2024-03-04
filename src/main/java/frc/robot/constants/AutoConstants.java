ead7e988cee4ad17a29697ea4854e6e0b0cd0458
NameTag1 <146138198+NameTag1@users.noreply.github.com>
2024-03-04 16:42:16 -0600

    Auto Shoulder Angle Changes
    Streamlined into one function that uses different constants, there are name changes, CHECK ALL AUTO COMMANDS.

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -4,7 +4,13 @@
 
 package frc.robot.constants;
 
+
 /** Add your docs here. */
 public final class AutoConstants {
-    
+    public static final double SUBWOOFER = 61;
+    public static final double SUBWOOFER_SIDE = 55;
+    public static final double CLOSE = 0;
+    public static final double ONE = 46.5;
+    public static final double TWO = 46.5;
+    public static final double THREE = 46.5;
 }
