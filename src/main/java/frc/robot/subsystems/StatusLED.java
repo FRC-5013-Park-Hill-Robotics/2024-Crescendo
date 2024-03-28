766ea4ece0528113cc10acbeb84da4ee5147b956
bradmongar <bradmongar@gmail.com>
2024-03-28 17:30:57 -0500

    Update StatusLED.java
    LED object creation

--- src/main/java/frc/robot/subsystems/StatusLED.java
+++ src/main/java/frc/robot/subsystems/StatusLED.java
@@ -21,17 +21,32 @@ import frc.robot.trobot5013lib.led.ChasePattern;
 import frc.robot.trobot5013lib.led.RainbowPattern;
 import frc.robot.trobot5013lib.led.SolidColorPattern;
 import frc.robot.trobot5013lib.led.TrobotAddressableLED;
+import frc.robot.trobot5013lib.led.TrobotAddressableLEDPattern;
 
 public class StatusLED extends SubsystemBase {
-   private TrobotAddressableLED m_AddressableLED = new TrobotAddressableLED(9, 39);
+  private TrobotAddressableLED m_AddressableLED = new TrobotAddressableLED(9, 39);
 
-   private Limelight mFrontLimelight;
-   private LauncherShoulder mShoulder;
-   private IntakeWrist mIntake;
+  private Limelight mFrontLimelight;
+  private LauncherShoulder mShoulder;
+  private IntakeWrist mIntake;
 
-   private Boolean mShuttling = false;
-   private String mPattern = "none";
-   
+  private Boolean mShuttling = false;
+  private String mPattern = "none";
+  private TrobotAddressableLEDPattern autoPattern = new ChasePattern(new Color[] { Color.kFirstRed, Color.kFirstBlue },
+      3);
+  private TrobotAddressableLEDPattern matchTimePattern = new RainbowPattern();
+  private TrobotAddressableLEDPattern shuttlingPattern = new BlinkingPattern(Color.kDarkRed, 0.1);
+  private TrobotAddressableLEDPattern ampPattern = new BlinkingPattern(Color.kPurple, 0.1);
+  private TrobotAddressableLEDPattern duckingPattern = new BlinkingPattern(Color.kDeepPink, 0.1);
+  private TrobotAddressableLEDPattern shotReadyPatterh = new SolidColorPattern(Color.kGreen);
+  private TrobotAddressableLEDPattern hasTargetPattern = new BlinkingPattern(Color.kGreen, 0.1);
+  private TrobotAddressableLEDPattern nonePattern = new BlinkingPattern(Color.kBlue, 0.1);
+  private TrobotAddressableLEDPattern disabledPattern = new ChasePattern(
+            new Color[] { Color.kDarkRed, Color.kOrangeRed, Color.kYellow, Color.kGreen, Color.kBlue, Color.kPurple },
+            1);
+  
+  
+
   /** Creates a new StatusLED. */
   public StatusLED(Limelight frontLL, LauncherShoulder shoulder, IntakeWrist wrist) {
     mFrontLimelight = frontLL;
@@ -46,72 +61,65 @@ public class StatusLED extends SubsystemBase {
     boolean isAutonomous = DriverStation.isAutonomous();
     double matchTime = DriverStation.getMatchTime();
 
-    if(isAutonomous && !isDisabled){
-      if(mPattern != "Auto Chaos"){
-        m_AddressableLED.setPattern(new ChasePattern(new Color[]{Color.kFirstRed, Color.kFirstBlue}, 3));
+    if (isAutonomous && !isDisabled) {
+      if (mPattern != "Auto Chaos") {
+        m_AddressableLED.setPattern(autoPattern);
       }
       mPattern = "Auto Chaos";
-    }
-    else if(!isDisabled){
-      if(matchTime >= 110 && matchTime <= 113){
-        if(mPattern != "Match Time"){
-          m_AddressableLED.setPattern(new RainbowPattern());
+    } else if (!isDisabled) {
+      if (matchTime >= 110 && matchTime <= 113) {
+        if (mPattern != "Match Time") {
+          m_AddressableLED.setPattern(matchTimePattern);
         }
         mPattern = "Match Time";
-      }
-      else if(mShuttling == true){
-        if(mPattern != "Shuttling"){
-          m_AddressableLED.setPattern(new BlinkingPattern(Color.kDarkRed, 0.1));
+      } else if (mShuttling == true) {
+        if (mPattern != "Shuttling") {
+          m_AddressableLED.setPattern(shuttlingPattern);
         }
         mPattern = "Shuttling";
-      } 
-      else if(mIntake.getGoal() == IntakeConstants.AMP_ANGLE_GROUND){
-        if(mPattern != "Amping"){
-          m_AddressableLED.setPattern(new BlinkingPattern(Color.kPurple, 0.1));
+      } else if (mIntake.getGoal() == IntakeConstants.AMP_ANGLE_GROUND) {
+        if (mPattern != "Amping") {
+          m_AddressableLED.setPattern(ampPattern);
         }
         mPattern = "Amping";
-      } 
-      else if(mShoulder.getGoal() == LauncherConstants.DUCK_RADIANS){
-        if(mPattern != "Ducking"){
-          m_AddressableLED.setPattern(new BlinkingPattern(Color.kDeepPink, 0.1));
+      } else if (mShoulder.getGoal() == LauncherConstants.DUCK_RADIANS) {
+        if (mPattern != "Ducking") {
+          m_AddressableLED.setPattern(duckingPattern);
         }
         mPattern = "Ducking";
-      } 
-      else if(mFrontLimelight.hasTarget() && mShoulder.atGoal() && mFrontLimelight.getVerticalAngleOfErrorDegrees() < 0){
-        if(mPattern != "Ready to Shoot"){
-          m_AddressableLED.setPattern(new SolidColorPattern(Color.kGreen));
+      } else if (mFrontLimelight.hasTarget() && mShoulder.atGoal()
+          && mFrontLimelight.getVerticalAngleOfErrorDegrees() < 0) {
+        if (mPattern != "Ready to Shoot") {
+          m_AddressableLED.setPattern(shotReadyPatterh);
         }
         mPattern = "Ready to Shoot";
-      } 
-      else if(mFrontLimelight.hasTarget()){
-        if(mPattern != "Has Target"){
-          m_AddressableLED.setPattern(new BlinkingPattern(Color.kGreen, 0.1));
+      } else if (mFrontLimelight.hasTarget()) {
+        if (mPattern != "Has Target") {
+          m_AddressableLED.setPattern(hasTargetPattern);
         }
         mPattern = "Has Target";
-      } 
-      else{
-        if(mPattern != "None"){
-        m_AddressableLED.setPattern(new BlinkingPattern(Color.kBlue, 0.1));
+      } else {
+        if (mPattern != "None") {
+          m_AddressableLED.setPattern(nonePattern);
         }
         mPattern = "None";
       }
 
       SmartDashboard.putString("LED Command", mPattern);
-    }
-    else{
-      if(mPattern != "Disabled Rainbow"){
-        m_AddressableLED.setPattern(new ChasePattern(new Color[]{Color.kDarkRed, Color.kOrangeRed, Color.kYellow, Color.kGreen, Color.kBlue, Color.kPurple}, 1));
+    } else {
+      if (mPattern != "Disabled Rainbow") {
+        m_AddressableLED.setPattern(disabledPattern);
       }
       mPattern = "Disabled Rainbow";
     }
   }
 
-  public void setShuttling (boolean truth){
+  public void setShuttling(boolean truth) {
     mShuttling = truth;
   }
 
-  public Command setShuttlingCommand (boolean truth){
-    Command theCommand = this.runOnce(()->{
+  public Command setShuttlingCommand(boolean truth) {
+    Command theCommand = this.runOnce(() -> {
       setShuttling(truth);
     });
     return theCommand;
